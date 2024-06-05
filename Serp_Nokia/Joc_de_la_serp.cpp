a60916289d321ac58e7b7dafb3738d2c7a373c4d
dafloresdp <149301894+dafloresdp@users.noreply.github.com>
2024-06-05 23:59:03 +0200

    Update Joc_de_la_serp.cpp

--- Serp_Nokia/Joc_de_la_serp.cpp
+++ Serp_Nokia/Joc_de_la_serp.cpp
@@ -6,59 +6,52 @@ using namespace miniwin;
 const int XTAM = 65, YTAM = 40;
 const int SZ = 12;
 
-void cuadrado(int x, int y, int c)
-{
+void cuadrado(int x, int y, int c) {
     color(c);
-    rectangulo_lleno(x*SZ, y*SZ, (x+1)*SZ-1, (y+1)*SZ-1);
+    rectangulo_lleno(x * SZ, y * SZ, (x + 1) * SZ - 1, (y + 1) * SZ - 1);
 }
-int main ()
-{
+
+int main() {
     vredimensiona(XTAM * SZ, YTAM * SZ);
-    int x = 30, y = 20; //Cabecita
-                    int vx = 1, vy = 0; //Speed
+    int x = 30, y = 20; // Cabecita
+    int vx = 1, vy = 0; // Speed
     int retraso = 0;
-    int t = tecla ();
-    while (t != ESCAPE)
-    {
+    int t = tecla();
+    while (t != ESCAPE) {
         retraso++;
-        //Proceso de teclas, lógica i tinte del juego
-        if (t== ARRIBA)
-        {
-
-            vx= 0, vy = -1;
-
-        }
-        else if (t==ABAJO)
-        {
-
-            vx=0, vy = 1;
-        }
-        else if(t==IZQUIERDA)
-        {
-
-            vx= -1, vy = 0;
-        }
-        else if (t==DERECHA)
-        {
-            vx = 1, vy = 0;
+        // Proceso de teclas, lógica y tinta del juego
+        if (t == ARRIBA) {
+            vx = 0; vy = -1;
+        } else if (t == ABAJO) {
+            vx = 0; vy = 1;
+        } else if (t == IZQUIERDA) {
+            vx = -1; vy = 0;
+        } else if (t == DERECHA) {
+            vx = 1; vy = 0;
         }
 
-        if (retraso == 8)
-        {
+        if (retraso == 8) {
             x += vx;
             y += vy;
-            retraso= 0;
+            retraso = 0;
+        }
+
+        // Verificación de límites
+        if (x < 0 || x >= XTAM || y < 0 || y >= YTAM) {
+            texto(10, 10, "Game Over :c");
+            refresca();
+            espera(2000); // Esperamos  2 segs para que se lea el mensaje 
+            break; // Salir
         }
 
         borra();
-        cuadrado (x,y, VERDE);
-        //Pinta todo
-        refresca ();
+        cuadrado(x, y, VERDE);
+        // Pinta todo
+        refresca();
         espera(10);
-        t=tecla();
+        t = tecla();
     }
     vcierra();
-
 
     return 0;
 }
